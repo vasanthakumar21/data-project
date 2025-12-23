a7717b53e9d06101c0d7ca5791d8f05249a40ae8
Vasanthakumar . <vasanthakumar.s@ganitinc.com>
2025-12-23 12:50:52 +0530

    data uploading with new scrpt

--- glue/etl.py
+++ glue/etl.py
@@ -4,17 +4,13 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-# Arguments passed from Lambda
 args = getResolvedOptions(
     sys.argv,
-    ['JOB_NAME', 'input_bucket', 'input_key']
+    ['JOB_NAME', 'input_path', 'output_path']
 )
 
-input_bucket = args['input_bucket']
-input_key = args['input_key']
-
-input_path = f"s3://{input_bucket}/{input_key}"
-output_path = "s3://kaggle-clean-bucket-vas/processed/"
+input_path = args['input_path']
+output_path = args['output_path']
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -23,13 +19,8 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# 🔹 Read CSV from S3
 df = spark.read.option("header", "true").csv(input_path)
-
-# 🔹 Simple cleaning
 clean_df = df.dropna().dropDuplicates()
-
-# 🔹 Write to S3 (folder auto-created)
 clean_df.write.mode("overwrite").parquet(output_path)
 
 job.commit()
