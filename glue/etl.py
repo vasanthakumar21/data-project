b186484a9da4b1b3056f76c7f06fa7e95e03b560
Vasanthakumar . <vasanthakumar.s@ganitinc.com>
2025-12-23 10:55:49 +0530

    trigger

--- glue/etl.py
+++ glue/etl.py
@@ -1,9 +1,9 @@
 df = spark.read.option("header", True).csv(
-    "s3://kaggle-raw-bucket/input/"
+    "s3://kaggle-raw-bucket-vas/input/"
 )
 
 clean_df = df.dropna().dropDuplicates()
 
 clean_df.write.mode("overwrite").parquet(
-    "s3://kaggle-clean-bucket/processed/"
+    "s3://kaggle-clean-bucket-vas/processed/"
 )
