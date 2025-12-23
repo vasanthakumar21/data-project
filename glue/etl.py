67f5d20fb3e83d1091ad9e1131e7db154f8e352a
Vasanthakumar . <vasanthakumar.s@ganitinc.com>
2025-12-23 12:36:29 +0530

    hiu

--- glue/etl.py
+++ glue/etl.py
@@ -1,9 +1,35 @@
-df = spark.read.option("header", True).csv(
-    "s3://kaggle-raw-bucket-vas/input/"
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+# Arguments passed from Lambda
+args = getResolvedOptions(
+    sys.argv,
+    ['JOB_NAME', 'input_bucket', 'input_key']
 )
 
+input_bucket = args['input_bucket']
+input_key = args['input_key']
+
+input_path = f"s3://{input_bucket}/{input_key}"
+output_path = "s3://kaggle-clean-bucket-vas/processed/"
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# 🔹 Read CSV from S3
+df = spark.read.option("header", "true").csv(input_path)
+
+# 🔹 Simple cleaning
 clean_df = df.dropna().dropDuplicates()
 
-clean_df.write.mode("overwrite").parquet(
-    "s3://kaggle-clean-bucket-vas/processed/"
-)
+# 🔹 Write to S3 (folder auto-created)
+clean_df.write.mode("overwrite").parquet(output_path)
+
+job.commit()
